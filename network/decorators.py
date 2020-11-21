d931baa7403358f73bc771a085a1e6969fee9a46
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-11-22 00:00:35 +0530

    Added APi for CREATE /connections

--- network/decorators.py
+++ network/decorators.py
@@ -1,5 +1,7 @@
 from .exceptions import CustomException
 from django.http import HttpResponse
+import json
+from .models import Device
 
 class Decorators():
 
@@ -22,6 +24,16 @@ class Decorators():
     def validateCommandContentType(self, function): 
         def innerFunction(*args, **kwargs): 
             body = args[1].body.decode("utf-8").split("\n")
+            body = [ele for ele in body if ele]
+            if body[0].split(" /")[0] == "FETCH":
+                return function(*args, **kwargs)
+            if len(body) <= 1: 
+                return HttpResponse(
+                    json.dumps(
+                        {"msg": "Invalid Command."}
+                    ),
+                    status=400
+                )
             commandContentType = body[1].split(" : ")[1]
             commandContentType = commandContentType.replace("\r", "")
             if commandContentType == "application/json": 
@@ -33,9 +45,95 @@ class Decorators():
 
     def validateCommandOperationTypes(self, function): 
         def innerFunction(*args, **kwargs):
+            command = args[1].body.decode("utf-8").split("\n")
+            command = [ele for ele in command if ele]
+            if command[0].split(" /")[0] == "FETCH":
+                return function(*args, *kwargs)
+            if len(command) <= 1: 
+                return HttpResponse(
+                    json.dumps(
+                        {"msg": "Invalid Command."}
+                    ),
+                    status=400
+                )
             operationType = args[1].body.decode("utf-8").split("\n")[0].split(" /")[0]
             if operationType in self.possibleOperationTypes:
                 return function(*args, *kwargs)
             else: 
                 return HttpResponse("Error", status=500)
+        return innerFunction
+
+
+    def checkIfConnectionsParamsValid(self, function): 
+        def innerFunction(*args, **kwargs):
+            params = json.loads(args[1][2])
+            if "source" in params and "targets" in params:
+                return function(*args, **kwargs)
+            else: 
+                return {
+                    "message": "Invalid command syntax",
+                    "status" : 400
+                }
+        return innerFunction
+
+    def checkIfSourceNodeExists(self, function):
+        def innerFunction(*args, **kwargs):
+            params = json.loads(args[1][2])
+            sourceNode = params["source"].strip()
+            if Device.objects.filter(deviceName=sourceNode).exists():
+                return function(*args, **kwargs)
+            else:
+                return {
+                    "message": "Node '{nodeName}' not found".format(nodeName=sourceNode),
+                    "status" : 400
+                }
+        return innerFunction
+
+    def checkIfSourceEqualsTargets(self, function):
+        def innerFunction(*args, **kwargs):
+            params = json.loads(args[1][2])
+            sourceNode = params["source"].strip()
+            targetNodes = params["targets"]
+            for target in targetNodes: 
+                if target.strip() == sourceNode:
+                    return {
+                    "message": "Cannot connect device to itself",
+                    "status" : 400
+                }
+                else:
+                    pass
+            return function(*args, **kwargs)
+        return innerFunction
+
+    def checkIfDevicesAreAlreadyConnected(self, function):
+        def innerFunction(*args, **kwargs):
+            params = json.loads(args[1][2])
+            sourceNode = params["source"].strip()
+            targetNodes = params["targets"]
+            sourceObj = Device.objects.get(deviceName=sourceNode)
+            allConnectedDevices  = sourceObj.connectedDevices.all()
+            for device in allConnectedDevices: 
+                if device.deviceName in targetNodes: 
+                    return {
+                        "message": "Devices are already connected",
+                        "status": 400
+                    }
+                else:
+                    pass
+            return function(*args, **kwargs)
+        return innerFunction
+
+    def checkIfTargetNodesExist(self, function):
+        def innerFunction(*args, **kwargs):
+            params = json.loads(args[1][2])
+            targetNodes = params["targets"]
+            for node in targetNodes:
+                if not Device.objects.filter(deviceName=node).exists():
+                    return {
+                        "message": "Node '{nodeName}' not found".format(nodeName=node),
+                        "status" : 400
+                    }
+                else:
+                    pass
+            return function(*args, **kwargs)
         return innerFunction
--- network/views.py
+++ network/views.py
@@ -15,32 +15,101 @@ class Process(View):
         command = commandText[1].replace("\r", "")
         return commandType, command
 
+    def getDeviceNameFromCommandText(self, commandText): 
+        commandText = commandText[2]
+        commandText = json.loads(commandText)
+        return commandText["name"]
+
     def isDeviceNameValid(self, deviceName): 
         return len(deviceName) > 1
 
     def checkDeviceType(self, deviceType):
         return deviceType in ["COMPUTER", "REPEATER"]
 
+    def getAllDevicesInTheNetwork(self): 
+        allDevices = list(Device.objects.all())
+        formattedDevices = [
+            {
+                "type": device.deviceType,
+                "name": device.deviceName
+            }
+            for device in allDevices
+        ]
+        return {
+            "devices": formattedDevices
+        }
+
+    def returnResponseDict(self, message, statusCode):
+        responseDict = dict()
+        responseDict["message"] = message
+        responseDict["status"] = statusCode
+        return responseDict
+
     def performFetchCommand(self, command, commandText):
         if command == "devices": 
-            pass
+            return self.getAllDevicesInTheNetwork()
+
+    def getDeviceObjectByDeviceName(self, sourceNode):
+        deviceObject = Device.objects.get(
+            deviceName =sourceNode.strip()
+        )
+        return deviceObject
+
+    @decorators.checkIfConnectionsParamsValid
+    @decorators.checkIfSourceNodeExists
+    @decorators.checkIfSourceEqualsTargets
+    @decorators.checkIfTargetNodesExist
+    @decorators.checkIfDevicesAreAlreadyConnected
+    def connectDevicesInTheNetwork(self, commandText): 
+        commandBody = json.loads(commandText[2])
+        if "source" in commandBody: 
+            sourceNode = commandBody["source"]
+            sourceObject = self.getDeviceObjectByDeviceName(sourceNode)
+            targetNodes = commandBody["targets"]
+            targetNodeObjs = Device.objects.filter(deviceName__in=targetNodes)
+            sourceObject.connectedDevices.add(*targetNodeObjs)
+            return self.returnResponseDict(
+                "Successfully connected",
+                200
+            )
         
-    def performCreateCommand(self, command, commandText):
-        if command == "devices": 
-            commandBody = commandText[2]
-            commandBody = json.loads(commandBody)
-            if self.isDeviceNameValid(commandBody["name"]) and self.checkDeviceType(commandBody["type"]):
+
+    def createDevice(self, commandText): 
+        commandBody = commandText[2]
+        commandBody = json.loads(commandBody)
+        if self.isDeviceNameValid(commandBody["name"]): 
+            if self.checkDeviceType(commandBody["type"]):
                 deviceObject = Device(
                     deviceName = commandBody["name"],
                     deviceType = commandBody["type"]
                 )
                 try: 
                     deviceObject.save()
+                    return self.returnResponseDict(
+                        "Successfully added {deviceName}".format(deviceName=commandBody["name"]),
+                        200
+                    )
                 except IntegrityError:
-                    return "error" 
-                return "ok"
-            else:
-                return "error"
+                    return self.returnResponseDict(
+                       "Device {deviceName} already exists".format(deviceName =commandBody["name"]),
+                        400
+                    )
+            else: 
+                return self.returnResponseDict(
+                       "type {deviceType} is not supported".format(deviceType =commandBody["type"]),
+                        400
+                )
+        else:
+             return self.returnResponseDict(
+                "Invalid Device Name",
+                400
+            )
+
+    def performCreateCommand(self, command, commandText):
+        if command == "devices": 
+            return self.createDevice(commandText)
+        elif command == "connections": 
+            return self.connectDevicesInTheNetwork(commandText)
 
     @decorators.validateRequestContentType
     @decorators.validateCommandContentType
@@ -49,18 +118,19 @@ class Process(View):
         commandText = request.body.decode("utf-8").split("\n")
         commandType, command = self.parseCommands(commandText)
         if commandType == "CREATE": 
-            if self.performCreateCommand(command, commandText) == "ok":
-                return HttpResponse(
-                    "Created Device Successfully",
-                    status=200
-                )
-            else: 
-                return HttpResponse(
-                    "Error while trying to create device, Possible Reasons are 1.) Maybe Device Name or Device Type format is invalid or Maybe a device already exists",
-                    status=500
-                )
+            responseDict = self.performCreateCommand(command, commandText)
+            return HttpResponse(
+                json.dumps(
+                    {"msg" : responseDict["message"]}
+                ),
+                status=responseDict["status"]
+            )
         elif commandType == "FETCH": 
             formattedRecords = self.performFetchCommand(command, commandText)
+            return HttpResponse(
+                json.dumps(formattedRecords),
+                status=200
+            )
         
         return HttpResponse("Ok")
 
